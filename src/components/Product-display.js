84e158b4ef5aca2381b97bc05cc789ee083eac9e
Ikezogwo Kosisochukwu <ikezogwokosiso16@gmail.com>
2024-06-30 10:25:40 +0100

    new changes to product

--- src/components/Product-display.js
+++ src/components/Product-display.js
@@ -3,10 +3,10 @@ import './Product-display.css';
 export function ProductDisplay(props){
     return(
         <div className="product-display">
-            <div className="product-card" id={props.id}>
-                <img className='product-image' src={props.image} alt="product" />
-                <h3>{props.name}</h3>
-                <p>${props.price}</p>
+            <div className="product-card">
+                <img className='product-image' src={props.image}alt="product" />
+                <h2>{props.name}</h2>
+                <div className='price'>${props.price}</div>
                 <button className='card-button'>Add to Bag</button>
             </div>
          </div>
