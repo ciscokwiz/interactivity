08845d87b00a1bdb72e49398088bb396db5219db
Ikezogwo Kosisochukwu <ikezogwokosiso16@gmail.com>
2024-06-30 10:47:06 +0100

    changes to the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,3 @@
-import { FaCartShopping } from "react-icons/fa6";
 import React, { useState }  from "react";
 import './Navbar.css';
 import { FaSearch } from "react-icons/fa";
@@ -19,10 +18,18 @@ function NavBar(){
             </div>
             <div className="nav-links">
                 <div className="nav-buttons">
-                    <a className='' href="/">Home</a>
-                    <a className='' href="product">Products</a>                        <i><a href=""><FaCartShopping /></a></i>
+                    <a className='nav-text' href="/">Home</a>
+                    <a className='nav-text' href="product">Products</a>
                     {search===true?<input className="searchinput" type="search" placeholder="Search" />:<div></div>}
-                    <button onClick={()=> setSearch(toggleValue = !toggleValue)}><i><a ><FaSearch /></a></i></button>
+                    <button onClick={()=> setSearch(toggleValue = !toggleValue)}><i><FaSearch className="search-icon" /></i></button>
+                    <div class="icon-cart">
+                        <svg  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+                        </svg>
+                         <span>0</span>
+              
+                    </div>
+                    
                 </div>
             </div>
         </div>
